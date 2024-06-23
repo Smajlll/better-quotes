0d880402574c9b523c4937169933783adffbca77
Vít Smolík <vit.smolik2@gmail.com>
2024-06-23 21:32:29 +0200

    standart react site, basic site functionality (eg. fetching the quote), dont mind the looks, its in the works :D

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [quote, setQuote] = useState('');
+  const [author, setAuthor] = useState('');
+
+  useEffect(() => {
+    
+    const url = 'https://thingproxy.freeboard.io/fetch/https://zenquotes.io/api/quotes';
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        setQuote(data[0].q);
+        setAuthor(data[0].a);
+      })
+      .catch(error => console.error('Error fetching quote:', error));
+  }, []); 
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className="top">
+      <h1>Better Quotes</h1>     
+      </div> 
+
+      <div className='center_qoute'>
+      <h2>{quote}</h2>
+      <h4>{author}</h4>
+      </div>
+
+      <div className='bottom'>
+        <button onClick={() => window.location.reload(false)}>New quote, pretty please.</button>
+      </div>
     </div>
   );
 }
